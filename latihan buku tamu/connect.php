5f33bd72b455343b8da8cd15ba5b094a63e43d60
ilham901 <81026543+ilham901@users.noreply.github.com>
2021-05-27 00:59:27 +0700

    Add files via upload

---
+++ latihan buku tamu/connect.php
@@ -0,0 +1,12 @@
+bukutamu-master<?php
+$hostmysql = "localhost";
+$username = "root";
+$password = "";
+$database = "datatamu";
+$conn = mysqli_connect ($hostmysql, $username, $password, $database);
+if ($conn){
+ echo "<b> Koneksi Berhasil </b>";
+}
+else{
+ die ("<b> Koneksi Gagal </b>");
+}
